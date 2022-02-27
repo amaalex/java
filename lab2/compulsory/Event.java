54c8620927c2dda2ba1377ebde563ed53543c478
Amarghioalei Alexandru IIIB5 <61457770+amaalex@users.noreply.github.com>
2022-02-27 15:31:16 +0200

    Add files via upload
    Uploading Lab2 Compulsory source code

---
+++ lab2/compulsory/Event.java
@@ -0,0 +1,63 @@
+package lab2.compulsory;
+
+/**
+ * this class builds how an Event should look like, based on specific attributes
+ */
+public class Event {
+    String name;
+    int size;
+    int startTime;
+    int endTime;
+
+    // constructors
+    Event(String name) {
+        this.name = name;
+    }
+
+    Event(String name, int size, int startTime, int endTime) {
+        this.name = name;
+        this.size = size;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    // getters and setters for attributes
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    //overriding toString method from Object class
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+}
---
+++ lab2/compulsory/Type.java
@@ -0,0 +1,10 @@
+package lab2.compulsory;
+
+/**
+ * Room Types
+ */
+
+public enum Type {
+    lecture_hall,
+    lab
+}

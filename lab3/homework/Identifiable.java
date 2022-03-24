ed211bc9b70259f9b4dbdd3bcec9bd54794362e0
Amarghioalei Alexandru IIIB5 <61457770+amaalex@users.noreply.github.com>
2022-03-24 08:32:29 +0200

    Delete Identifiable.java

--- lab3/homework/Identifiable.java
+++
@@ -1,9 +0,0 @@
-package lab3.homework;
-
-/**
- * Interface with abstract methods which must be implemented
- */
-public interface Identifiable {
-    public abstract String getIP();
-    public abstract void setIP(String ip);
-}

b6190468b7c6ab26e445fe2cc473157eacb85b79
alexandruamarghioalei <alexamarghioalei@icloud.com>
2022-04-27 00:13:58 +0300

    Commit for Bonus part

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - Lab8:
   - Compulsory - ![](https://us-central1-progress-markdown.cloudfunctions.net/progress/100)
   - Homework - ![](https://us-central1-progress-markdown.cloudfunctions.net/progress/100)
-  - Bonus - ![](https://us-central1-progress-markdown.cloudfunctions.net/progress/0)
+  - Bonus - ![](https://us-central1-progress-markdown.cloudfunctions.net/progress/25)
 
 - Lab7:
   - Compulsory - ![](https://us-central1-progress-markdown.cloudfunctions.net/progress/100)
---
+++ lab8/src/main/java/bonus/Database.java
@@ -0,0 +1,22 @@
+package bonus;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+/**
+ * this class initiates a connection using Hikari
+ */
+public class Database {
+    private static HikariDataSource dataSource = null;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/speedy");
+        config.setUsername("speedy");
+        config.setPassword("");
+        config.addDataSourceProperty("minimumIdle", "5");
+        config.addDataSourceProperty("maximumPoolSize", "25");
+
+        Database.dataSource = new HikariDataSource(config);
+    }
+}
